4208f35f27d8138014af09724d74b36645a568e3
Ndeta Innocent <you@example.com>
2022-07-10 23:50:08 +0300

    creating first ecommerce schema with graphql

--- server.js
+++ server.js
@@ -5,16 +5,65 @@ const app = express()
 const PORT = 3000
 const schema = buildSchema(`
 type Query {
-    description:String
-    price:Float
-}`)
+    products:[Product]
+    orders:[Order]
+}
+    type Product{
+        id:ID!
+        description:String!
+        reveiws:[Review]
+        price:Float!
+    }
+    type Review {
+        rating:Int!
+        comment:String
+    }
+    
+    type Order{
+        date:String!
+        subtotal:Float!
+        items:[OrderItem]
+    }
+
+    type OrderItem {
+        product:Product!
+        quantity:Int!
+    }
+`)
 const root = {
-    description: 'Red shoe',
-    price: 43.2
+    products: [
+        {
+            id: 'redshoe',
+            description: 'Red shoe',
+            price: 32.3
+        },
+        {
+            id: 'bluejeans',
+            description: 'Blue jeans',
+            price: 93.3
+        }
+    ],
+    orders: [
+        {
+            date: '2005-04-03',
+            subtotal: 90.4,
+            items: [
+                {
+                    product: {
+                        id: 'redshoe',
+                        description: 'Old red shoe',
+                        price: 66.6
+                    },
+                    quantity: 3
+                }
+            ]
+        }
+    ]
 }
 app.use('/graphql', graphqlHTTP({
     schema: schema,
-    rootValue: root
+    rootValue: root,
+    graphiql: true
 }))
 app.listen(PORT, () => {
     console.log(`Running graphql server on port ${PORT}`)
