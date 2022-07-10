9688654d7716c09a8bd97a794bc85df2c5f6f3b0
Ndeta Innocent <you@example.com>
2022-07-11 00:21:50 +0300

    modelling project to make it cleaner

---
+++ orders/orders.model.js
@@ -0,0 +1,16 @@
+module.exports = [
+    {
+        date: '2005-04-03',
+        subtotal: 90.4,
+        items: [
+            {
+                product: {
+                    id: 'redshoe',
+                    description: 'Old red shoe',
+                    price: 66.6
+                },
+                quantity: 3
+            }
+        ]
+    }
+]
---
+++ products/product.model.js
@@ -0,0 +1,12 @@
+module.exports = [
+    {
+        id: 'redshoe',
+        description: 'Red shoe',
+        price: 32.3
+    },
+    {
+        id: 'bluejeans',
+        description: 'Blue jeans',
+        price: 93.3
+    }
+]
